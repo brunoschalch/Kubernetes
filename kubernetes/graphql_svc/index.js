390bd8314e6af37fc9c5b9b6b91080914dc655da
Angel Cueto <angelcueto96@outlook.com>
2019-05-12 17:27:17 -0500

    Basic scheme structure ready

--- kubernetes/graphql_svc/index.js
+++ kubernetes/graphql_svc/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const graphqlHTTP = require('express-graphql');
-var {buildSchema} = require('graphql');
+const {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema
+} = require('graphql');
+
 const axios = require('axios');
 const app = express()
 
@@ -16,33 +24,138 @@ if(DEVMODE) {
   process.env.AUTH_SVC_URI = 'http://localhost:8086'
 }
 
+var fabricantes = [
+     {
+        "id":"Don Julio",
+        "desc":"En 1942222, la pasión por el tequila impulsó a nuestro fundador Don Julio González a mejorar cada aspecto de este tequila —desde la cosecha hasta el embotellado— para elaborar el tequila más prestigioso del mundo. Hecho 100% de Blue Weber Agave.",
+        "carac":"Guiado por su pasión, Don Julio González se inspiró para producir el tequila más fino revolucionando todo el proceso: desde la plantación y cosecha del agavetequilana Weber Variedad Azul hasta la forma de la botella.",
+        "foto":"https://www.donjulio.com/images/home_tequila_bottle.jpg"
+     },
+     {
+        "id":"Cazadores",
+        "desc":"En 1922, en Arandas, México, Don José María Bañuelos se asomó a su ventana y contempló las colinas plagadas de agave azul. Lo que vio, entre la arcilla roja y el agave azul, fue un ciervo, valientemente parado en medio del campo.",
+        "carac":"Se dice que después de que Don José perfeccionara su nuevo tequila Cazadores, ocultó la receta entre las paredes de su casa, salvaguardándola como su posesión más preciada. Durante 51 años, Cazadores ha permanecido como un secreto de familia.",
+        "foto":"https://d1oqiickzzpl3c.cloudfront.net/wp-content/uploads/2017/07/09120315/Painted-Wall-min-1.jpg"
+     },
+     {
+        "id":"Corralejo",
+        "desc":"En la tierra donde nació el cura Hidalgo se encuentra Tequilera Corralejo, ubicada en una bella Hacienda bajo el mismo nombre que se localiza en el municipio de Pénjamo, Guanajuato.",
+        "carac":"El tour en la Hacienda Corralejo se inicia después de leer un letrero que da la bienvenida y refleja la calidad humana de la empresa ofreciendo al visitante satisfacer su curiosidad y expectación acerca de los procesos que se siguen para obtener el tequila.",
+        "foto":"https://tequilacorralejo.mx/assets/base/img/home/slider/slider-corralejo-1.png"
+     }
+  ]
+
 // GraphQL Schema
-const schema = buildSchema(`
-    "The root of it all"
-    type Query {
-        "Returns a greeting"
-        hello: String
-    }
-`);
 
-const root = {
-  hello: () => {
-      return 'Hello World!';
+const FabricanteType = new GraphQLObjectType({ // complex type (with fields)
+  name: 'Fabricante',
+  fields: {
+      id: {
+          type: new GraphQLNonNull(GraphQLString) 
+      },
+      desc: {
+          type: new GraphQLNonNull(GraphQLString) 
+      },
+      carac: {
+          type: new GraphQLNonNull(GraphQLString) 
+      },
+      foto: {
+          type: new GraphQLNonNull(GraphQLString)
+      }
   }
-}
+});
 
-app.use('/graphql', graphqlHTTP({
-  schema,             // .. using our schema
-  rootValue: root,    // .. and our resolvers
-  graphiql: true,     // .. and the GraphiQL editor - wait for it!
-}));
+const TequilaType = new GraphQLObjectType({ 
+  name: 'Tequila',
+  fields: {
+      id: {
+          type: new GraphQLNonNull(GraphQLString) 
+      },
+      marca: {
+          type: new GraphQLNonNull(GraphQLString) 
+      },
+      submarca: {
+          type: new GraphQLNonNull(GraphQLString) 
+      },
+      desc: {
+        type: new GraphQLNonNull(GraphQLString) 
+      },
+      carac: {
+        type: new GraphQLList(GraphQLString) //Should be string Array
+      },
+
+      foto: {
+          type: new GraphQLNonNull(GraphQLString)
+      }
+  }
+});
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+      fabricantes: {
+          type: new GraphQLList(FabricanteType), // a list of users
+          resolve: () => {
+              return fabricantes;
+          }
+      },
+      fabricante: {
+          type: TequilaType,
+          args: {
+              id: {
+                  type: new GraphQLNonNull(GraphQLString)
+              }
+          },
+          /* in this notation, the resolver function takes the
+           * parent object as first parameter, and the arguments
+           * as second.
+           */
+          resolve: (user, args) => { 
+              //TODO: return fabricante with id == args.id
+              return fabricantes.findOneById(args.id);
+          }
+      },
+      tequilas: {
+        type: new GraphQLList(TequilaType), // a list of users
+        resolve: () => {
+            //TODO: Return a list with all tequilas
+            return fabricantes;
+        }
+      },
+      tequila: {
+        type: TequilaType,
+        args: {
+            id: {
+                type: new GraphQLNonNull(GraphQLString)
+            }
+        },
+        /* in this notation, the resolver function takes the
+         * parent object as first parameter, and the arguments
+         * as second.
+         */
+        resolve: (user, args) => { 
+            //TODO: return Tequila with id == args.id
+            return fabricantes.findOneById(args.id);
+        }
+    },
+
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: QueryType
+});
+
+
 
 
 
 // Todas las rutas deben empezar con /api/graphql/. Fuera de eso no hay reglas.
+
 app.get("/api/graphql/:id", async (req, res, next) => {
   try {
-    const id = parseInt(req.params.id)
+    //const id = parseInt(req.params.id)
+    const id = 23
 
 // Make a request for a user with a given ID
 axios.get(process.env.TEQUILA_SVC_URI+'/api/tequila/'+id)
@@ -62,6 +175,14 @@ axios.get(process.env.TEQUILA_SVC_URI+'/api/tequila/'+id)
   }
 
 })
+
+
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
 
 port = process.env.PORT || 8080
 
