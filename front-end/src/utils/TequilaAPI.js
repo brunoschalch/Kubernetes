270631829bed1b0e3fb9c45869bece83caa8ed65
Angel Cueto <angelcueto96@outlook.com>
2019-05-14 21:33:28 -0500

    code refactor

--- front-end/src/utils/TequilaAPI.js
+++ front-end/src/utils/TequilaAPI.js
@@ -27,21 +27,9 @@ function get(tequila) {
 }
 
 function getGql(tequila){
-  const httpLink = createHttpLink({
-    uri: 'http://localhost:8080/graphql',
-  });
   
-  const authLink = setContext((_, { headers }) => {
-    // get the authentication token from local storage if it exists
-    //const token = localStorage.getItem('token');
-    // return the headers to the context so httpLink can read them
-    return {
-      headers: {
-        ...headers,
-        authorization: 'joe:password' ,
-      }
-    }
-  });
+  
+  
   
 
   var client = new ApolloClient({
